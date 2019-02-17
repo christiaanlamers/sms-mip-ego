dda4ec13644425ac5a719a7be53a0ebc254c1a50
Christiaan Lamers <christiaanlamers@gmail.com>
2019-02-17 21:41:45 +0100

    testing simple mnist network

--- all_cnn_bi_skippy.py
+++ all_cnn_bi_skippy.py
@@ -415,7 +415,7 @@ def test_skippy():
     #test parameters
     #original parameters
     #RESnet-34-like
-    stack_0 = 0#1
+    stack_0 = 4#1
     stack_1 = 0#6
     stack_2 = 0#4
     stack_3 = 0#4
