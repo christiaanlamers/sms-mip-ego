da98a903ec57b9646ab6d72039cd023388f3412f
Christiaan Lamers <christiaanlamers@gmail.com>
2019-03-25 11:47:57 +0100

    again a restart

--- load_analysis.py
+++ load_analysis.py
@@ -17,6 +17,8 @@ import math
 
 from scipy.interpolate import UnivariateSpline
 
+import numbers
+
 file_name = str(sys.argv[1])
 with open(file_name) as f:
     for line in f:
@@ -57,34 +59,55 @@ for i in range(len(conf_array)):
     solutions.append(Solution(x=conf_x,fitness=fit_array[i],n_eval=n_eval_array[i],index=index_array[i],var_name=name_array[i],loss=loss_array[i],time=time_array[i]))
 
 print("len(solutions): " + str(len(solutions)))
-print([i.to_dict() for i in solutions])
+#print([i.to_dict() for i in solutions])
 #y = [np.exp(-i.loss) for i in solutions]#[i.time for i in solutions]
-n_points = 1000
+data_lib = {}
+#y = [np.exp(-i.loss) for i in solutions]#[i.time for i in solutions]
+data_lib['acc'] = [np.exp(-i.loss) for i in solutions]#[i.time for i in solutions]
+data_lib['time'] = [i.time for i in solutions]
+#n_points = 1000
+stopper = 0
 for i in name_array[0]:
     print(i)
     x = []
-    y = []
+    #y = []
     for j in solutions:
         #if j.to_dict()['lr'] < 0.01:
         x.append(j.to_dict()[i])
-        y.append(np.exp(-j.loss))
+        #y.append(np.exp(-j.loss))
+    if isinstance(x[0], numbers.Number):
+        data_lib[i] = x
     #print(x)
-    try:
-        plt.xlabel(i)
-        plt.ylabel('accuracy')
-        #plt.ylabel('time')
-        plt.plot(x,y, 'o')
-    except:
-        print('could not plot data')
-    try:
-        spl = UnivariateSpline(x, y)
-        max_x = max(x)
-        x_model = [k*max_x/n_points for k in range(n_points)]
-        y_model = [spl(k) for k in x_model]
-        plt.plot(x_model,y_model,color="red")
-    except:
-        print('could not fit a spline')
-    try:
-        plt.show()
-    except:
-        pass
+    #try:
+    #    plt.xlabel(i)
+    #    plt.ylabel('accuracy')
+    #    #plt.ylabel('time')
+    #    plt.plot(x,y, 'o')
+    #except:
+    #    print('could not plot data')
+    #try:
+    #    spl = UnivariateSpline(x, y)
+    #    max_x = max(x)
+    #    x_model = [k*max_x/n_points for k in range(n_points)]
+    #    y_model = [spl(k) for k in x_model]
+    #    plt.plot(x_model,y_model,color="red")
+    #except:
+    #    print('could not fit a spline')
+    #try:
+    #    plt.show()
+    #except:
+    #    pass
+
+data_panda = pd.DataFrame(data=data_lib)
+
+#g = sns.PairGrid(data_panda,vars=['acc','time','lr','l2','s_0','s_1','filters_0','filters_1','k_0','k_1','dropout_0','dropout_1','dense_size_0','dense_size_1'],hue='acc',palette='GnBu_d')
+#g = sns.PairGrid(data_panda,vars=['filters_0','filters_1','filters_2','filters_3','filters_4','filters_5','filters_6','filters_7','filters_8','filters_9','filters_10','filters_11','filters_12','filters_13'],hue='acc',palette='GnBu_d')
+g = sns.PairGrid(data_panda,vars=['k_0','k_1','k_2','k_3','k_4','k_5','k_6','k_7','k_8','k_9','k_10','k_11','k_12','k_13'],hue='acc',palette='GnBu_d')
+g.map(plt.scatter)
+#g.map_upper(plt.scatter)
+#g.map_lower(sns.kdeplot)
+#g.map_diag(sns.kdeplot, lw=3, legend=False);
+
+#plt.savefig('load_analysis_output.png')
+plt.savefig('load_analysis_output_kernels.png')
+#plt.show()
