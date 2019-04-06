65904c70019a7cd24132215ac69b68409b21b2ca
Christiaan Lamers <christiaanlamers@gmail.com>
2019-04-06 20:02:24 +0200

    restart

--- mnist_configurator_bi_skippy_cut.py
+++ mnist_configurator_bi_skippy_cut.py
@@ -133,7 +133,7 @@ class obj_func(object):
 
 
 #define the search space.
-save_name = '../../../data/s0315435/data_skippy_cifar10_big_one_cut_smaller'
+save_name = '../../../data/s0315435/data_skippy_cifar10_big_one_cut_smaller_restart_1'
 objective = obj_func('./all_cnn_bi_skippy_cut.py',save_name=save_name)
 activation_fun = ["softmax"]
 activation_fun_conv = ["elu","relu","tanh","sigmoid","selu"]
@@ -208,8 +208,8 @@ opt = mipego(search_space, objective, time_model, loss_model, ftarget=None,
 
 #incumbent, stop_dict = opt.run() #CHRIS opt.run() does not return anything anymore
 #CHRIS restart code
-if False:
-    with open('flipflap.json') as f:
+if True:
+    with open('data_skippy_cifar10_big_one_cut_smaller_intermediate.json') as f:
         for line in f:
             data = json.loads(line)
 
