68cf65a254c0143f70bc8864617983e99639ce05
Christiaan Lamers <christiaanlamers@gmail.com>
2019-05-15 02:35:11 +0200

    skippy4 now without projection

--- all_cnn_bi_skippy_4.py
+++ all_cnn_bi_skippy_4.py
@@ -81,7 +81,7 @@ class Skip_manager(object):
                     #CHRIS can funcion as addition, or projection followed by addition
                     #padded = Conv2D(filters, (1,1), padding='same', kernel_regularizer=l2(regulizer), bias_regularizer=l2(regulizer))(padded)#CHRIS kernel value set to (1,1) in order to simply act as projection
                     if K.int_shape(padded)[3] < K.int_shape(layer)[3]:
-                        padded = tf.pad(padded, [[0, 0], [np.ceil((K.int_shape(layer)[3]- K.int_shape(padded)[3])/ 2), np.floor((K.int_shape(layer)[3]- K.int_shape(padded)[3])/ 2)], [0, 0], [0, 0]])
+                        padded = tf.pad(padded, [[0, 0], [int(np.ceil((K.int_shape(layer)[3]- K.int_shape(padded)[3])/ 2)), int(np.floor((K.int_shape(layer)[3]- K.int_shape(padded)[3])/ 2))], [0, 0], [0, 0]])
                     else:
                         layer = tf.pad(layer, [[0, 0], [np.ceil((K.int_shape(padded)[3] -K.int_shape(layer)[3])/ 2), np.floor((K.int_shape(padded)[3] - K.int_shape(layer)[3])/ 2)], [0, 0], [0, 0]])
                 layer = Add()([layer, padded])
@@ -110,17 +110,17 @@ class Skip_manager(object):
                     #CHRIS can funcion as addition, or projection followed by addition
                     #padded = Conv2D(filters, (1,1), padding='same', kernel_regularizer=l2(regulizer), bias_regularizer=l2(regulizer))(padded)#CHRIS kernel value set to (1,1) in order to simply act as projection
                     if K.int_shape(padded)[3] < K.int_shape(layer)[3]:
-                        padded = tf.pad(padded, [[0, 0], [np.ceil((K.int_shape(layer)[3]- K.int_shape(padded)[3])/ 2), np.floor((K.int_shape(layer)[3]- K.int_shape(padded)[3])/ 2)], [0, 0], [0, 0]])
+                        padded = tf.pad(padded, [[0, 0], [int(np.ceil((K.int_shape(layer)[3]- K.int_shape(padded)[3])/ 2)), int(np.floor((K.int_shape(layer)[3]- K.int_shape(padded)[3])/ 2))], [0, 0], [0, 0]])
                     else:
-                        layer = tf.pad(layer, [[0, 0], [np.ceil((K.int_shape(padded)[3] -K.int_shape(layer)[3])/ 2), np.floor((K.int_shape(padded)[3] - K.int_shape(layer)[3])/ 2)], [0, 0], [0, 0]])
+                        layer = tf.pad(layer, [[0, 0], [int(np.ceil((K.int_shape(padded)[3] -K.int_shape(layer)[3])/ 2)), int(np.floor((K.int_shape(padded)[3] - K.int_shape(layer)[3])/ 2))], [0, 0], [0, 0]])
                 layer= Add()([padded, incoming_layer])
         else:
             if K.int_shape(incoming_layer)[3] != filters:
                 #CHRIS convolution to bound amount of features
                 #CHRIS can funcion as addition, or projection followed by addition
-                incoming_layer = Conv2D(filters, (1,1), padding='same', kernel_regularizer=l2(regulizer), bias_regularizer=l2(regulizer))(incoming_layer)#CHRIS kernel value set to (1,1) in order to simply act as projection
+                #incoming_layer = Conv2D(filters, (1,1), padding='same', kernel_regularizer=l2(regulizer), bias_regularizer=l2(regulizer))(incoming_layer)#CHRIS kernel value set to (1,1) in order to simply act as projection
                 if K.int_shape(incoming_layer)[3] < K.int_shape(layer)[3]:
-                    incoming_layer = tf.pad(incoming_layer, [[0, 0], [np.ceil((K.int_shape(layer)[3]- K.int_shape(incoming_layer)[3])/ 2), np.floor((K.int_shape(layer)[3]- K.int_shape(incoming_layer)[3])/ 2)], [0, 0], [0, 0]])
+                    incoming_layer = tf.pad(incoming_layer, [[0, 0], [int(np.ceil((K.int_shape(layer)[3]- K.int_shape(incoming_layer)[3])/ 2)), int(np.floor((K.int_shape(layer)[3]- K.int_shape(incoming_layer)[3])/ 2))], [0, 0], [0, 0]])
                 else:
                     layer = tf.pad(layer, [[0, 0], [np.ceil((K.int_shape(incoming_layer)[3] -K.int_shape(layer)[3])/ 2), np.floor((K.int_shape(incoming_layer)[3] - K.int_shape(layer)[3])/ 2)], [0, 0], [0, 0]])
             layer= Add()([layer, incoming_layer])
